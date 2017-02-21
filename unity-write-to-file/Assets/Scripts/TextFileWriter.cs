75ab0595a3da0ed1269599cf20d0a9959a68f78c
alvaro <alvarolinarez9@gmail.com>
2017-02-21 08:00:01 -0500

    update text writer example

---
+++ unity-write-to-file/Assets/Scripts/TextFileWriter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class TextFileWriter : MonoBehaviour {
+
+    public const string FILE_PATH = "logs.txt";
+
+
+	void Start () {
+	}
+	
+	void Update () {
+		
+	}
+
+    public void WriteLine(string line)
+    {
+        File.AppendAllText(FILE_PATH, line + "\n");
+    }
+
+    public void Clear()
+    {
+        File.Delete(FILE_PATH);
+    }
+}
